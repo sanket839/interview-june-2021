a05ce7e6b75ec1b6d8b6770e441d2b8fb36abd32
sanket839 <57365168+sanket839@users.noreply.github.com>
2021-06-22 12:30:22 +0530

    Update ShiftElementsInArray.java

--- src/main/java/exam/ShiftElementsInArray.java
+++ src/main/java/exam/ShiftElementsInArray.java
@@ -3,7 +3,19 @@ package exam;
 public class ShiftElementsInArray {
 
   public void shift(int[] arr) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
+            int j, last;    
+           
+            last = arr[arr.length-1];    
+            
+            for(j = arr.length-1; j > 0; j--){    
+                 
+                arr[j] = arr[j-1];    
+            }    
+            arr[0] = last;    
+            for(i=0;i<arr.length;i++){
+              System.out.println(arr[i]);
+
+            }
 
   }
 
